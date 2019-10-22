a015d05ba25e1b934da41803527b034ac1276804
Wang <wangb@shure.com>
2019-10-22 14:57:45 +0800

    Simple update
    Signed-off-by: Wang <wangb@shure.com>

--- bhtx_python/ben_test_bhtx_command_repeat.py
+++ bhtx_python/ben_test_bhtx_command_repeat.py
@@ -10,13 +10,14 @@ if __name__ == "__main__":
 
     # @todo Arg parsing here could use some improvement...
     parser = argparse.ArgumentParser(description='Run BHTX integration tests')
-    parser.add_argument('-n','--hostname', help='Hostname for CLI connection')
+    parser.add_argument('-n', '--hostname', help='Hostname for CLI connection')
     # Set default COM=6
-    parser.add_argument('-p','--port', default = 'COM6', help='Port for CLI connection')
+    parser.add_argument('-p', '--port', default='COM6', help='Port for CLI connection')
     # Set default baudrate=115200
-    parser.add_argument('-b','--baud', default = 115200, help='Baud for CLI connection')
+    parser.add_argument('-b', '--baud', default=115200, help='Baud for CLI connection')
     # Set default IR = True
-    parser.add_argument('--noir', dest='ir', action='store_const', const=False, default=True, help='Specify that serial connection is NOT IR')
+    parser.add_argument('--noir', dest='ir', action='store_const', const=False, default=True,
+                        help='Specify that serial connection is NOT IR')
     args = parser.parse_args()
 
     # Establish connection to transmitter
@@ -31,19 +32,19 @@ if __name__ == "__main__":
     bhtx.send_cmd("verapp")
     bhtx.send_cmd("su dev dev")
 
-    for i in range(1,1000):
-       bhtx.send_cmd("factoryReset")
-       bhtx.send_cmd("reboot",expect_resp=False) # reboot the device 
-       time.sleep(4) # delay 4 seconds
-       bhtx.send_cmd("su mfr Rj1146uHCpsCHakrBKQ")
-       bhtx.send_cmd("serialnum")
-       bhtx.send_cmd("txtype")
-       bhtx.send_cmd("ateuid")
-       bhtx.send_cmd("verpkg")
-       bhtx.send_cmd("verboot")
-       bhtx.send_cmd("verapp")
-       bhtx.send_cmd("verfpga")
-       bhtx.send_cmd("rebootfpga 2")
-       print('test ran %d times\n' %i )
-       if ( i == 1000 ):
-           print('Finished! test ran %d times\n' %i )
+    for i in range(1, 1000):
+        bhtx.send_cmd("su mfr Rj1146uHCpsCHakrBKQ")
+        bhtx.send_cmd("serialnum")
+        bhtx.send_cmd("txtype")
+        bhtx.send_cmd("ateuid")
+        bhtx.send_cmd("verpkg")
+        bhtx.send_cmd("verboot")
+        bhtx.send_cmd("verapp")
+        bhtx.send_cmd("verfpga")
+        bhtx.send_cmd("rebootfpga 2")
+        bhtx.send_cmd("factoryReset")
+        bhtx.send_cmd("reboot", expect_resp=False)  # reboot the device
+        time.sleep(3)  # delay 3 seconds
+        print('test ran %d times\n' % i)
+        if i == 1000:
+            print('Finished! test ran %d times\n' % i)
