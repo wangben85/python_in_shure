d7b6beca8e851d5afcac1daf66a2918604f75131
wang <wangb@shure.com>
2020-10-29 23:13:54 +0800

    modify the script to add the 00 in the end of each line
    Signed-off-by: wang <wangb@shure.com>

--- datTobin/datTobin.py
+++ datTobin/datTobin.py
@@ -5,8 +5,8 @@ import os
 
 import struct
 
-file_dat = 'ble_led3_init.dat'
-file_bin = 'ble_led3_init.bin'
+file_dat = 'ble_app_blinky_LED3_init.dat'
+file_bin = 'ble_app_blinky_LED3_init.bin'
 
 fn_dat = open(file_dat, "rb")
 fn_bin = open(file_bin, "ab+")
@@ -14,6 +14,7 @@ fn_bin = open(file_bin, "ab+")
 size = os.path.getsize(file_dat)
 print(size)
 
+data0 = struct.pack('B', 0)
 L = []
 j = 0
 for i in range(size):
@@ -23,8 +24,11 @@ for i in range(size):
     a = struct.pack('B', value[0])
     fn_bin.write(a)
     j = j + 1
-    if ( j == 16 ):
+    if ( j == 15 ):
        j = 0
+       fn_bin.write(data0)
        print('\n')
+#fn_bin.write(data0)    # need to check whether it is need or not
+
 fn_dat.close()
 fn_bin.close()
