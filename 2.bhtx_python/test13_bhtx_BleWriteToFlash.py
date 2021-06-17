1d8b513aa415e5e4c63cb7cf98da8068faede9d2
wangben85 <wangben85@sina.com>
2021-06-17 09:44:59 +0800

    update the program the adress for slxd3
    Signed-off-by: wangben85 <wangben85@sina.com>

--- 2.bhtx_python/test13_bhtx_BleWriteToFlash.py
+++ 2.bhtx_python/test13_bhtx_BleWriteToFlash.py
@@ -1,5 +1,5 @@
 """
-This test is to program FreqTable file to external flash with python script
+This test is to program BLE image file to external flash with python script
 
 Note: If we are using this script in high loading device(e.g. ADX3 with ShowLink) , Before run this script,
       ShowLink needs to be disabled first
@@ -21,8 +21,10 @@ if __name__ == "__main__":
     parser.add_argument('--noir', dest='ir', action='store_const', const=False, default=True)
    
     # Specify the BLE image file name
+    # image 1, app and init
     #parser.add_argument('-f', '--file', default='./BLE_Images/ble_app_data.bin')
     #parser.add_argument('-f', '--file', default='./BLE_Images/ble_app_init.dat')
+    # image2, app and init
     #parser.add_argument('-f', '--file', default='./BLE_Images/ble_app_uart_slave_data.bin')
     parser.add_argument('-f', '--file', default='./BLE_Images/ble_app_uart_slave_init.dat')
 
@@ -36,12 +38,24 @@ if __name__ == "__main__":
 
     tx.unlock()  # switch to dev privilege mode
 
-    #ExternalFlashStartAddr = 0x00280000    # debgu only , Backup FPGA image location start addr in external flash
     ExternalFlashSectorSize = 4 * 1024     # external flash sector size is 4K Bytes
+    #test on AD3
+    #image1 App size is 15K, init packet size is 3K( 0x284800 - 0x283c00)
     #ExternalFlashStartAddr  = 0x00280000   # Ble image file location start addr, ble_app_data
     #ExternalFlashStartAddr  = 0x00283c00   # Ble image file location start addr, ble_app_init
+    #image2 size is 30K, init packet size is 3K ( 0x28c000 - 0x28cc00)
     #ExternalFlashStartAddr  = 0x00284800   # Ble image file location start addr, ble_app_uart_data
     ExternalFlashStartAddr  = 0x0028c000   # Ble image file location start addr, ble_app_uart_init
+
+    #test on SLXD3
+    #image1 App size is 15K, init packet size is 3K( 0x1A4800 - 0x1A3c00)
+    #ExternalFlashStartAddr  = 0x001A0000   # Ble image file location start addr, ble_app_data
+    #ExternalFlashStartAddr  = 0x001A3c00   # Ble image file location start addr, ble_app_init
+    #image2 size is 30K, init packet size is 3K ( 0x1Ac000 - 0x1Acc00)
+    #ExternalFlashStartAddr  = 0x001A4800   # Ble image file location start addr, ble_app_uart_data
+    #ExternalFlashStartAddr  = 0x001Ac000   # Ble image file location start addr, ble_app_uart_init
+
+    #
     FlashAddrIndex = ExternalFlashStartAddr   # Address index starts from ExternalFlashStartAddr
     
     print("If run this script on ADX3, make sure the ShowLink network is disable");  
@@ -64,9 +78,9 @@ if __name__ == "__main__":
     tx.send_cmd("flashpuke 0x{:x} 16".format(ExternalFlashStartAddr))
 
 
-    binfile = open(args.file, 'rb')           # open the FreqTable file
-    size = os.path.getsize(args.file)         # get the FreqTable file size
-    print("BLE image file size is {}".format(size))  # print the FreqTable file size
+    binfile = open(args.file, 'rb')           # open the BLE image file
+    size = os.path.getsize(args.file)         # get the BLE image file size
+    print("BLE image file size is {}".format(size))  # print the BLE image file size
 
     numofSector = int(size / ExternalFlashSectorSize) + 1    # calculate how many sectors does BLE image file occupies in external flash
     print("sector number is {}".format(numofSector))         # print the sector numbers
@@ -75,12 +89,12 @@ if __name__ == "__main__":
         ExternalFlashStartAddr = ExternalFlashStartAddr + ExternalFlashSectorSize
 
     for i in range(size):
-        data = binfile.read(1)                  # get each byte from the BLE image file
-        value = struct.unpack('B',data)         # convert to integer
+        data = binfile.read(1)                   # get each byte from the BLE image file
+        value = struct.unpack('B',data)          # convert to integer
             #print(value[0])                     # convert to hex and print
             #print(str(value[0]))                # convert to string and print
         tx.send_cmd("flashpoke 0x{:x} {}".format(FlashAddrIndex, str(value[0])))
-        FlashAddrIndex = FlashAddrIndex + 1     # Address index increaments
+        FlashAddrIndex = FlashAddrIndex + 1      # Address index increaments
 
-    tx.send_cmd("reboot", expect_resp=False)    # after all the FreqBand file, reboot the device
+    tx.send_cmd("reboot", expect_resp=False)     # after all the BLE image file, reboot the device
 
